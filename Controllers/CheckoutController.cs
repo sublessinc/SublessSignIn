8e4f2e133094f82a7aa7ab81877c981651cd3b7c
jon blanton <jon@subless.com>
2021-04-21 20:27:59 -0400

    Added configuration management

--- Controllers/CheckoutController.cs
+++ Controllers/CheckoutController.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Stripe;
+using Stripe.Checkout;
 using SublessSignIn.Models;
 
 namespace SublessSignIn.Controllers
@@ -13,10 +13,19 @@ namespace SublessSignIn.Controllers
     [ApiController]
     public class CheckoutController : ControllerBase
     {
-        IOptions<StripeConfig> _stripeConfig { get; set; }
+        private readonly IOptions<StripeConfig> _stripeConfig;
+        private readonly IStripeClient _client;
+
         public CheckoutController(IOptions<StripeConfig> stripeConfig)
-        {
-            _stripeConfig = stripeConfig;
+        {            
+            _stripeConfig = stripeConfig ?? throw new ArgumentNullException(nameof(stripeConfig));
+            _ = stripeConfig.Value.PublishableKey ?? throw new ArgumentNullException(nameof(stripeConfig.Value.PublishableKey));
+            _ = stripeConfig.Value.BasicPrice ?? throw new ArgumentNullException(nameof(stripeConfig.Value.BasicPrice));
+            _ = stripeConfig.Value.Domain ?? throw new ArgumentNullException(nameof(stripeConfig.Value.Domain));
+            _ = stripeConfig.Value.SecretKey ?? throw new ArgumentNullException(nameof(stripeConfig.Value.SecretKey));
+            _ = stripeConfig.Value.WebhookSecret ?? throw new ArgumentNullException(nameof(stripeConfig.Value.WebhookSecret));
+            _client = new StripeClient(_stripeConfig.Value.SecretKey);
+
         }
         [HttpGet("setup")]
         public StripeCheckoutViewModel Setup()
@@ -26,7 +35,45 @@ namespace SublessSignIn.Controllers
                 BasicPrice = _stripeConfig.Value.BasicPrice,
                 PublishableKey = _stripeConfig.Value.PublishableKey,
             };
+
         }
-    }
+
+        [HttpPost("create-checkout-session")]
+        public async Task<IActionResult> CreateCheckoutSession([FromBody] CreateCheckoutSessionRequest req)
+        {
+            var options = new SessionCreateOptions
+            {
+                SuccessUrl = $"{_stripeConfig.Value.Domain}/success.html?session_id={{CHECKOUT_SESSION_ID}}",
+                CancelUrl = $"{_stripeConfig.Value.Domain}/canceled.html",
+                PaymentMethodTypes = new List<string>
+                {
+                    "card",
+                },
+                Mode = "subscription",
+                LineItems = new List<SessionLineItemOptions>
+                {
+                    new SessionLineItemOptions
+                    {
+                        Price = req.PriceId,
+                        Quantity = 1,
+                    },
+                },
+            };
+
+            var service = new SessionService(_client);
+            try
+            {
+                var session = await service.CreateAsync(options);
+                return Ok(new CreateCheckoutSessionResponse
+                {
+                    SessionId = session.Id,
+                });
+            }
+            catch (StripeException e)
+            {
+                Console.WriteLine(e.StripeError.Message);
+                return BadRequest();
+            }
+        }
 
 }
--- Models/StripeConfig.cs
+++ Models/StripeConfig.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SublessSignIn.Models
+﻿namespace SublessSignIn.Models
 {
     public class StripeConfig
     {
