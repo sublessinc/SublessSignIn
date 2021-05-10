65d210bbc28fcb67679ff14e4376fda05adf9984
jon blanton <jon@subless.com>
2021-05-10 18:43:52 -0400

    added the redist

--- SublessSignIn/Controllers/PaymentController.cs
+++ SublessSignIn/Controllers/PaymentController.cs
@@ -1,34 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace SublessSignIn.Controllers
 {
     [Route("api/[controller]")]
-    public class PaymentController : ControllerBase
+    public class HitController : ControllerBase
     {
-        public PaymentController()
+        private ILogger _logger;
+        public HitController(ILoggerFactory loggerFactory)
         {
-            Console.WriteLine("I'm trying");
+            _logger = loggerFactory?.CreateLogger<HitController>() ?? throw new ArgumentNullException(nameof(loggerFactory));
         }
 
-        [HttpPost("pay")]
+        [HttpPost]
         [EnableCors("Unrestricted")]
         [Authorize]
-        public void Setup(Hit hit)
+        public async Task<ActionResult> Hit()
         {
-
-            Console.WriteLine($"Hit from : {User.FindFirst("cognito:username")?.Value} on {hit.currentUrl}");
+            using (var reader = new StreamReader(Request.Body))
+            {
+                var body = await reader.ReadToEndAsync();
+                if (string.IsNullOrWhiteSpace(body))
+                {
+                    _logger.LogWarning("Invalid hit data recieved:{Environment.NewLine}" +
+                       $"Http Request Information:{Environment.NewLine}" +
+                       $"Schema:{Request.Scheme} " +
+                       $"Host: {Request.Host} " +
+                       $"Path: {Request.Path} " +
+                       $"QueryString: {Request.QueryString}");
+                    return BadRequest("No url included in hit");
+                }
+                if (!Uri.TryCreate(body, UriKind.RelativeOrAbsolute, out Uri hitSource))
+                {
+                    return BadRequest("Could not read source url");
+                }
+            }
+            return Ok();
         }
-
-        public class Hit
-        {
-            public string currentUrl { get; set; }
-        }
-
     }
 }
---
+++ SublessSignIn/wwwroot/dist/subless.js
@@ -0,0 +1,143 @@
+﻿var myHeaders = new Headers();
+myHeaders.set('Cache-Control', 'no-store');
+var urlParams = new URLSearchParams(window.location.search);
+
+var baseURI = location.protocol + '//' + window.location.hostname + (location.port ? ':' + location.port : '');
+var redirectURI = baseURI;
+
+
+
+//Convert Payload from Base64-URL to JSON
+const decodePayload = payload => {
+    const cleanedPayload = payload.replace(/-/g, '+').replace(/_/g, '/');
+    const decodedPayload = atob(cleanedPayload)
+    const uriEncodedPayload = Array.from(decodedPayload).reduce((acc, char) => {
+        const uriEncodedChar = ('00' + char.charCodeAt(0).toString(16)).slice(-2)
+        return `${acc}%${uriEncodedChar}`
+    }, '')
+    const jsonPayload = decodeURIComponent(uriEncodedPayload);
+
+    return JSON.parse(jsonPayload)
+}
+
+//Parse JWT Payload
+const parseJWTPayload = token => {
+    const [header, payload, signature] = token.split('.');
+    const jsonPayload = decodePayload(payload)
+
+    return jsonPayload
+};
+
+//Parse JWT Header
+const parseJWTHeader = token => {
+    const [header, payload, signature] = token.split('.');
+    const jsonHeader = decodePayload(header)
+
+    return jsonHeader
+};
+
+//Generate a Random String
+const getRandomString = () => {
+    const randomItems = new Uint32Array(28);
+    crypto.getRandomValues(randomItems);
+    const binaryStringItems = randomItems.map(dec => `0${dec.toString(16).substr(-2)}`)
+    return binaryStringItems.reduce((acc, item) => `${acc}${item}`, '');
+}
+
+//Encrypt a String with SHA256
+const encryptStringWithSHA256 = async str => {
+    const PROTOCOL = 'SHA-256'
+    const textEncoder = new TextEncoder();
+    const encodedData = textEncoder.encode(str);
+    return crypto.subtle.digest(PROTOCOL, encodedData);
+}
+
+//Convert Hash to Base64-URL
+const hashToBase64url = arrayBuffer => {
+    const items = new Uint8Array(arrayBuffer)
+    const stringifiedArrayHash = items.reduce((acc, i) => `${acc}${String.fromCharCode(i)}`, '')
+    const decodedHash = btoa(stringifiedArrayHash)
+
+    const base64URL = decodedHash.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+    return base64URL
+}
+
+// Main Function
+async function main(settings) {
+    var code = urlParams.get('code');
+    var token = sessionStorage.getItem('id_token');
+
+    //If code not present then request code else request tokens
+    if (code == null && token == null) {
+
+        // Create random "state"
+        var state = getRandomString();
+        sessionStorage.setItem("pkce_state", state);
+
+        // Create PKCE code verifier
+        var code_verifier = getRandomString();
+        sessionStorage.setItem("code_verifier", code_verifier);
+
+        // Create code challenge
+        var arrayHash = await encryptStringWithSHA256(code_verifier);
+        var code_challenge = hashToBase64url(arrayHash);
+        sessionStorage.setItem("code_challenge", code_challenge)
+
+        // Redirtect user-agent to /authorize endpoint
+        location.href = settings.issuerUrl + "/oauth2/authorize?response_type=code&state=" + state + "&client_id=" + settings.appClientId + "&redirect_uri=" + redirectURI + "&scope=openid&code_challenge_method=S256&code_challenge=" + code_challenge;
+    }
+        if (code!= null && token == null)  {
+
+            // Verify state matches
+            state = urlParams.get('state');
+            if (sessionStorage.getItem("pkce_state") != state) {
+                alert("Invalid state");
+            } else {
+
+                // Fetch OAuth2 tokens from Cognito
+                code_verifier = sessionStorage.getItem('code_verifier');
+                await fetch(settings.issuerUrl + "/oauth2/token?grant_type=authorization_code&client_id=" + settings.appClientId + "&code_verifier=" + code_verifier + "&redirect_uri=" + redirectURI + "&code=" + code, {
+                    method: 'post',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    }
+                })
+                    .then((response) => {
+                        return response.json();
+                    })
+                    .then((data) => {
+                        tokens = data;
+                        sessionStorage.setItem("id_token", tokens.id_token);
+                        sessionStorage.setItem("access_token", tokens.access_token);
+                    });
+            }
+        }
+}
+
+fetch("http://localhost:7070/api/Authorization/settings")
+    .then(function (resp) {
+        var json = resp.json().then(json => {
+            main(json);
+        });
+    });
+
+
+function phoneHome() {
+    var token = sessionStorage.getItem('id_token');
+    if (token) {
+        var body = 
+        fetch("http://localhost:7070/api/hit", {
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer " + token,
+                "Content-Type": "application/json",
+            },
+            body: window.location.href
+        })
+            .then(function (resp) {
+                console.log("Phoned home");
+            });
+    }
+}
+
+phoneHome();
