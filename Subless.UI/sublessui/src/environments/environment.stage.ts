d2b20ff3f981c2729688a4fcb0b2c1be2ae2903d
jon blanton <jon@subless.com>
2021-08-25 20:24:33 -0400

    Fixed build script to reflect environments

--- Subless.UI/sublessui/src/environments/environment.stage.ts
+++ Subless.UI/sublessui/src/environments/environment.stage.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  authority: 'https://cognito-idp.us-east-2.amazonaws.com/us-east-2_OUauzAEmb',
-  clientId: '509u5gthsqgim8esui2peqi3d8'
+  authority: 'https://cognito-idp.us-east-2.amazonaws.com/us-east-2_QDv13spV2',
+  clientId: '45092m5aqv0rism5fsduf19030'
 };
--- promoteToAws.sh
+++ promoteToAws.sh
@@ -1,12 +1,12 @@
 cd ./Subless.UI/sublessui
-npm run-script build
+ng build --configuration dev && cp -r ./dist/sublessui/* ../../SublessSignIn/wwwroot
 cd ./../../
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 548668861663.dkr.ecr.us-east-1.amazonaws.com
-docker build -t subless-pay .
-docker tag subless-pay:latest 548668861663.dkr.ecr.us-east-1.amazonaws.com/subless-pay:latest
-docker push 548668861663.dkr.ecr.us-east-1.amazonaws.com/subless-pay:latest
+docker build -t subless-pay:dev .
+docker tag subless-pay:dev 548668861663.dkr.ecr.us-east-1.amazonaws.com/subless-pay:dev
+docker push 548668861663.dkr.ecr.us-east-1.amazonaws.com/subless-pay:dev
 
 
-docker build -f CalculatorDockerfile . -t subless-calculator
-docker tag subless-calculator:latest 548668861663.dkr.ecr.us-east-1.amazonaws.com/subless-calculator:latest
-docker push 548668861663.dkr.ecr.us-east-1.amazonaws.com/subless-calculator:latest
+docker build -f CalculatorDockerfile . -t subless-calculator:dev
+docker tag subless-calculator:dev 548668861663.dkr.ecr.us-east-1.amazonaws.com/subless-calculator:dev
+docker push 548668861663.dkr.ecr.us-east-1.amazonaws.com/subless-calculator:dev
