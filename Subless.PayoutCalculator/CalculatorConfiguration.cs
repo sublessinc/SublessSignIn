6cee4447f36bc48e4184b6fd521ce647ac91c8e8
jon blanton <jon@subless.com>
2021-11-08 20:55:44 -0500

    Added summary

--- Subless.PayoutCalculator/CalculatorConfiguration.cs
+++ Subless.PayoutCalculator/CalculatorConfiguration.cs
@@ -4,6 +4,9 @@ namespace Subless.PayoutCalculator
 {
     public class CalculatorConfiguration
     {
+        /// <summary>
+        /// Remote storage bucket name. S3 Bucket.
+        /// </summary>
         public string BucketName;
         public bool RunOnStart;
         // Did this instead of timespan due to months being a wonky division, and not available for some calculations
