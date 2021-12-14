de8ccbf694a307ca3eabbb89fa15ede05b0ad079
jon blanton <jon@subless.com>
2021-12-13 20:48:54 -0500

    Code cleanup

--- Subless.JS/src/subless.ts
+++ Subless.JS/src/subless.ts
@@ -1,4 +1,3 @@
-import { UserManager, Log, UserManagerSettings } from "oidc-client-ts";
 
 const subless_Headers = new Headers();
 subless_Headers.set('Cache-Control', 'no-store');
@@ -8,66 +7,41 @@ const client_baseUri = location.protocol + '//' + window.location.hostname + (lo
 const client_currentPath = window.location.pathname;
 
 interface SublessInterface {
-    subless_GetConfig(): Promise<UserManagerSettings>;
+    subless_GetConfig(): Promise<SublessSettings>;
     sublessLogin(): Promise<void>;
     subless_LoggedIn(): Promise<boolean>;
     subless_hit(): Promise<void>;
     sublessLogout(): Promise<void>;
 }
 
-var subless_config: UserManagerSettings = {
+interface SublessSettings {
+    redirect_uri: string;
+    post_logout_redirect_uri: string;
+    authority: string;
+    client_id: string;
+}
+
+var subless_config: SublessSettings = {
     redirect_uri: client_baseUri,
     post_logout_redirect_uri: client_baseUri,
-
-
-    // these two will be done dynamically from the buttons clicked, but are
-    // needed if you want to use the silent_renew
-    response_type: "code",
-    scope: "openid",
-
-    // this will toggle if profile endpoint is used
-    loadUserInfo: true,
-
-    // silent renew will get a new access_token via an iframe 
-    // just prior to the old access_token expiring (60 seconds prior)
-    //silent_redirect_uri: window.URL.createObjectURL(subless_silentRenewBlob),
-    automaticSilentRenew: true,
-
-    // will revoke (reference) access tokens at logout time
-    revokeAccessTokenOnSignout: true,
-
-    // this will allow all the OIDC protocol claims to be visible in the window. normally a client app 
-    // wouldn't care about them or want them taking up space
-    filterProtocolClaims: false,
     authority: "",
-    client_id: "",
-    metadataSeed: {}
-};
+    client_id: ""
 
-var callbackRunning: Promise<void> = null;
+};
 
 
 export class Subless implements SublessInterface {
-    subless_Config: Promise<UserManagerSettings>;
-    subless_UserManager: Promise<UserManager>;
+    subless_Config: Promise<SublessSettings>;
     constructor() {
         this.subless_Config = this.subless_GetConfig();
         this.subless_hit();
     }
 
-    async subless_GetConfig(): Promise<UserManagerSettings> {
+    async subless_GetConfig(): Promise<SublessSettings> {
         var resp = await fetch(subless_Uri + "/api/Authorization/settings");
         var json = await resp.json();
         subless_config.authority = json.cognitoUrl;
         subless_config.client_id = json.appClientId;
-        subless_config.metadataSeed = {
-            end_session_endpoint:
-                json.issuerUrl
-                + "/logout?response_type=code&client_id="
-                + subless_config.client_id
-                + "&logout_uri="
-                + client_baseUri
-        };
         return subless_config;
     }
 
