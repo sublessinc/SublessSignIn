e29937bacf70ee12dd45b666d5ddc21d2c6ebb7b
jon blanton <jon@subless.com>
2022-01-23 09:54:45 -0500

    Added a health check endpoint

---
+++ SublessSignIn/Controllers/HealthCheckController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SublessSignIn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        private readonly IHealthCheck healthCheck;
+        private readonly ILogger logger;
+
+        public HealthCheckController(IHealthCheck healthCheck, ILoggerFactory loggerFactory)
+        {
+            this.healthCheck = healthCheck ?? throw new ArgumentNullException(nameof(healthCheck));
+            this.logger = loggerFactory?.CreateLogger<IHealthCheck>() ?? throw new ArgumentNullException(nameof(loggerFactory));
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<ActionResult<bool>> IsHealthy()
+        {
+            bool isHealthy = false;
+            try
+            {
+                isHealthy = await healthCheck.IsHealthy();
+            }
+            catch (Exception e)
+            {
+                logger.LogCritical(e, "HealthCheck failure");
+            }
+            if (isHealthy)
+            {
+                return Ok(true);
+            }
+            return StatusCode(503, false);
+        }
+    }
+}
---
+++ SublessSignIn/HealthCheck.cs
@@ -0,0 +1,35 @@
+﻿using Subless.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SublessSignIn
+{
+    public class HealthCheck : IHealthCheck
+    {
+        private readonly IAdministrationService administrationService;
+        private readonly IStripeService stripeService;
+
+        public HealthCheck(IAdministrationService administrationService, IStripeService stripeService)
+        {
+            this.administrationService = administrationService ?? throw new ArgumentNullException(nameof(administrationService));
+            this.stripeService = stripeService ?? throw new ArgumentNullException(nameof(stripeService));
+        }
+
+        public async Task<bool> IsHealthy()
+        {
+            return await CanAccessDatabase() && await CanAccessStripe();
+        }
+        private async Task<bool> CanAccessDatabase()
+        {
+            return await administrationService.CanAccessDatabase();
+        }
+
+        private async Task<bool> CanAccessStripe()
+        {
+            return await stripeService.CanAccessStripe();
+        }
+
+    }
+}
