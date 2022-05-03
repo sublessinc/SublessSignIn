ee1b01a9027496793a7dee8e214e25fad7448504
jon blanton <jon@subless.com>
2022-05-03 11:51:17 -0400

    Added ef storage for data protection keys so they can be shared between instances

---
+++ Subless.Data/DataProtectionKeyStorage/DataProtectionContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using static Subless.Data.DataDi;
+
+namespace Subless.Data
+{
+    public class DataProtectionContextFactory : IDesignTimeDbContextFactory<KeyStorageContext>
+    {
+        public DataProtectionContextFactory()
+        {
+            // A parameter-less constructor is required by the EF Core CLI tools.
+        }
+
+        public KeyStorageContext CreateDbContext(string[] args)
+        {
+            var credsJson = Environment.GetEnvironmentVariable("dbCreds");
+            if (string.IsNullOrWhiteSpace(credsJson))
+            {
+                throw new ArgumentNullException("dbCreds must be provided to generate migrations");
+            }
+            var dbCreds = JsonConvert.DeserializeObject<DbCreds>(credsJson);
+
+            var optionsBuilder = new DbContextOptionsBuilder<KeyStorageContext>();
+            optionsBuilder.UseNpgsql(dbCreds.GetDatabaseConnection());
+            return new KeyStorageContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Subless.Data/DataProtectionKeyStorage/KeyStorageContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Subless.Data
+{
+    public class KeyStorageContext : DbContext, IDataProtectionKeyContext
+    {
+        // A recommended constructor overload when using EF Core 
+        // with dependency injection.
+        public KeyStorageContext(DbContextOptions<KeyStorageContext> options)
+            : base(options) { }
+
+        // This maps to the table that stores keys.
+        public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
+    }
+}
