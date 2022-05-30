659de6cafd77e31d3deeda2fa8bdaf5e48d17ed6
jon blanton <jon@subless.com>
2022-05-30 15:06:32 -0400

    Added comment

--- Subless.UI.Tests/Keys/Keys.py
+++ Subless.UI.Tests/Keys/Keys.py
@@ -7,6 +7,8 @@ class Keys:
 
     mailslurp_api_key = all_json['mailslurp_api_key']
     god_token = all_json['god_token']  # ha ha this is no longer valid
+    # I assume kyle is referring to us switching from jwt based authentication to cookies that exist for a session
+    # lifetime. That probably will require some re-vetting of assumptions whenever I find where this was used.
     god_password = all_json['god_password']
     god_email = all_json['god_email']
     cognito_app_client_id = all_json['cognito_app_client_id']
