f34d93b67103299463d0c99a6d27b640f48e32da
jon blanton <jon@subless.com>
2022-06-02 18:17:22 -0400

    Restructured configuration management

---
+++ Subless.Configuration/CalculatorSettingsConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Subless.PayoutCalculator;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subless.Configuration
+{
+    public static class CalculatorSettingsConfiguration
+    {
+        public static IServiceCollection RegisterCalculatorConfig(IServiceCollection services)
+        {
+            services.Configure<CalculatorConfiguration>(options =>
+            {
+                options.BucketName = Environment.GetEnvironmentVariable("BucketName") ?? throw new ArgumentNullException("BucketName");
+                options.ExecutionsPerYear = int.Parse(Environment.GetEnvironmentVariable("ExecutionsPerYear") ?? throw new ArgumentNullException("ExecutionsPerYear"), CultureInfo.InvariantCulture);
+                options.RunOnStart = bool.Parse(Environment.GetEnvironmentVariable("RunOnStart") ?? "false");
+                options.CalcuationRangeEnd = Environment.GetEnvironmentVariable("CalcuationRangeEnd");
+                options.CalcuationRangeStart = Environment.GetEnvironmentVariable("CalcuationRangeStart");
+                options.Domain = Environment.GetEnvironmentVariable("DOMAIN");
+                options.PoolId = Environment.GetEnvironmentVariable("DOMAIN");
+            });
+            return services;
+        }
+    }
+}
--- Subless.Services/Services.DI.cs
+++ Subless.Services/Services.DI.cs
@@ -9,18 +9,7 @@ namespace Subless.Services
     {
         public static IServiceCollection AddServicesDi(IServiceCollection services)
         {
-            services.Configure<StripeConfig>(options =>
-            {
-                options.PublishableKey = Environment.GetEnvironmentVariable("STRIPE_PUBLISHABLE_KEY") ?? throw new ArgumentNullException("STRIPE_PUBLISHABLE_KEY");
-                options.SecretKey = Environment.GetEnvironmentVariable("STRIPE_SECRET_KEY") ?? throw new ArgumentNullException("STRIPE_SECRET_KEY");
-                options.WebhookSecret = Environment.GetEnvironmentVariable("STRIPE_WEBHOOK_SECRET") ?? throw new ArgumentNullException("STRIPE_WEBHOOK_SECRET");
-                options.Domain = Environment.GetEnvironmentVariable("DOMAIN") ?? throw new ArgumentNullException("DOMAIN");
-                if (!options.Domain.EndsWith('/'))
-                {
-                    options.Domain += '/';
-                }
-                options.SublessPayPalId = Environment.GetEnvironmentVariable("PayPalId");
-            });
+
             services.Configure<DomainConfig>(options =>
             {
                 options.Domain = Environment.GetEnvironmentVariable("DOMAIN") ?? throw new ArgumentNullException("DOMAIN");
