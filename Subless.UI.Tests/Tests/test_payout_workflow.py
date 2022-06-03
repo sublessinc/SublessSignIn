dd2860c9398c11945bdaeb8ee67e0f4fe6ed1546
jon blanton <jon@subless.com>
2022-06-03 13:39:30 -0400

    Fixed assertion syntax

--- Subless.UI.Tests/Tests/test_payout_workflow.py
+++ Subless.UI.Tests/Tests/test_payout_workflow.py
@@ -21,7 +21,10 @@ def test_payout_calculation(web_driver, subless_activated_creator_user, paying_u
     id, cookie = login_as_god_user(web_driver)
     endTime = datetime.utcnow()
     calculator_result = get_payout_calculation(cookie, startTime, endTime)
-    assert calculator_result is not None
+    payments = calculator_result['allPayouts'].values()
+    assert 0.89 in payments
+    assert 0.09 in payments
+    assert 3.56 in payments
 
 
 
