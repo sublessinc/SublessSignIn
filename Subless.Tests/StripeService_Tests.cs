7f737047456919581e263bae41a6e21a78730032
jon blanton <jon@subless.com>
2022-06-26 18:57:29 -0400

    Got some tests in place

--- Subless.Tests/StripeService_Tests.cs
+++ Subless.Tests/StripeService_Tests.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
 using Stripe;
+using Subless.Models;
 using Subless.Services.Services;
 using System;
 using System.Collections.Generic;
@@ -22,9 +23,74 @@ namespace Subless.Tests
             sut.GetPayersForRange(new DateTimeOffset(), new DateTimeOffset());
         }
 
+        [Fact]
+        public void GetPayers_WithOneInvoiceAndNoUsers_ReturnsNoPayers()
+        {
+            var invoices = new List<Invoice>() { new Invoice() };
+            var sut = StripeServiceBuilder.BuildStripeService(invoices);
+            var payers = sut.GetPayersForRange(new DateTimeOffset(), new DateTimeOffset());
+            Assert.Empty(payers);
+        }
+
+        [Fact]
+        public void GetPayers_WithOneInvoiceAndOneUser_ReturnsOnePayer()
+        {
+            var stripeId = "stripeId";
+            var invoices = new List<Invoice>() { new Invoice() { CustomerId = stripeId } };
+            var users = new List<User>() { new User() { StripeCustomerId = stripeId, Id = Guid.NewGuid() } };
+            var sut = StripeServiceBuilder.BuildStripeService(invoices, users);
+            var payers = sut.GetPayersForRange(new DateTimeOffset(), new DateTimeOffset());
+            Assert.Single(payers);
+            Assert.Equal(users.Single().Id, payers.Single().UserId);
+        }
+
+        [Fact]
+        public void GetPayers_WithInvoiceAndFullRefund_ReturnsNoPayment()
+        {
+            var stripeId = "stripeId";
+            var chargeId = "chargeId";
+            var invoices = new List<Invoice>() { new Invoice() { CustomerId = stripeId, ChargeId = chargeId} };
+            var charge = new Charge() { Id = chargeId,  Amount = 1000 };
+            var refund = new Refund() { ChargeId = chargeId, Amount = 1000 };
+            var users = new List<User>() { new User() { StripeCustomerId = stripeId, Id = Guid.NewGuid() } };
+            var sut = StripeServiceBuilder.BuildStripeService(
+                invoices, 
+                users, 
+                new List<Refund> { refund} , 
+                new List<Charge> { charge },
+                new BalanceTransaction());
+            var payers = sut.GetPayersForRange(new DateTimeOffset(), new DateTimeOffset());
+            Assert.Single(payers);
+            Assert.Equal(users.Single().Id, payers.Single().UserId);
+            Assert.Equal(0, payers.Single().Payment);
+        }
+        [Fact]
+        public void GetPayers_WithInvoiceWithFees_ReturnsFeesInPayment()
+        {
+            var stripeId = "stripeId";
+            var chargeId = "chargeId";
+            var invoices = new List<Invoice>() { new Invoice() { CustomerId = stripeId, ChargeId = chargeId } };
+            var charge = new Charge() { Id = chargeId, Amount = 1000 };
+            var users = new List<User>() { new User() { StripeCustomerId = stripeId, Id = Guid.NewGuid() } };
+            var sut = StripeServiceBuilder.BuildStripeService(
+                invoices,
+                users,
+                new List<Refund> { },
+                new List<Charge> { charge },
+                new BalanceTransaction(){ Fee = 97 });
+            var payers = sut.GetPayersForRange(new DateTimeOffset(), new DateTimeOffset());
+            Assert.Single(payers);
+            Assert.Equal(users.Single().Id, payers.Single().UserId);
+            Assert.Equal(97, payers.Single().Fees);
+        }
         public static class StripeServiceBuilder 
         {
-            public static StripeService BuildStripeService()
+            public static StripeService BuildStripeService(
+                List<Invoice> testInvoices = null,
+                List<User> users = null,
+                List<Refund> refunds = null,
+                List<Charge> charges = null,
+                BalanceTransaction balanceTransaction = null)
             {
                 var serviceProvider = new ServiceCollection()
                     .AddLogging(x =>
@@ -35,9 +101,28 @@ namespace Subless.Tests
                 var factory = serviceProvider.GetService<ILoggerFactory>();
                 var stripeWrapper = new Mock<IStripeApiWrapperService>();
                 var invoiceService = new Mock<InvoiceService>();
-                invoiceService.Setup(x => x.List(It.IsAny<InvoiceListOptions>(), null)).Returns(new StripeList<Invoice>());
-                stripeWrapper.Setup(x=>x.InvoiceService).Returns(invoiceService.Object);
-                var sut = new StripeService(Options.Create(new Models.StripeConfig()), Mock.Of<IUserService>(), stripeWrapper.Object, factory);
+                var invoices = new StripeList<Invoice>() { Data = testInvoices ?? new List<Invoice>() };
+                invoiceService.SetupSequence(x => x.List(It.IsAny<InvoiceListOptions>(), null))
+                    .Returns(invoices)
+                    .Returns(new StripeList<Invoice>() { Data = new List<Invoice>() }); // Need two returns in sequence to account for paging
+                stripeWrapper.Setup(x => x.InvoiceService).Returns(invoiceService.Object);
+                var userService = new Mock<IUserService>();
+                userService.Setup(x => x.GetUsersFromStripeIds(It.IsAny<IEnumerable<string>>())).Returns(users ?? new List<User>());
+                var refundService = new Mock<RefundService>();
+                refundService.Setup(x => x.List(It.IsAny<RefundListOptions>(), null))
+                    .Returns(new StripeList<Refund> { Data = refunds ?? new List<Refund>() });
+                stripeWrapper.Setup(x => x.RefundService).Returns(refundService.Object);
+                var chargeService = new Mock<ChargeService>();
+                chargeService.Setup(x => x.List(It.IsAny<ChargeListOptions>(), null))
+                    .Returns(new StripeList<Charge> { Data = charges ?? new List<Charge>() });
+                chargeService.Setup(x => x.Get(It.IsAny<string>(), null, null))
+                .Returns(charges?.First());
+                stripeWrapper.Setup(x => x.ChargeService).Returns(chargeService.Object);
+                var balanceTransactionService = new Mock<BalanceTransactionService>();
+                balanceTransactionService.Setup(x => x.Get(It.IsAny<string>(), null, null))
+                    .Returns(balanceTransaction);
+                stripeWrapper.Setup(x => x.BalanceTransactionService).Returns(balanceTransactionService.Object);
+                var sut = new StripeService(Options.Create(new Models.StripeConfig()), userService.Object, stripeWrapper.Object, factory);
                 return sut;
             }
         }
