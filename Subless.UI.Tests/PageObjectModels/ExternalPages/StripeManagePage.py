0831a6a9db7102c427d000b74ae8ae95a77354a3
jon blanton <jon@subless.com>
2022-07-25 16:37:30 -0400

    fixed inconsistent plan locator

--- Subless.UI.Tests/PageObjectModels/ExternalPages/StripeManagePage.py
+++ Subless.UI.Tests/PageObjectModels/ExternalPages/StripeManagePage.py
@@ -1,4 +1,6 @@
 # todo:  fill this out
+from selenium.common.exceptions import NoSuchElementException
+
 from PageObjectModels.BasePage import BasePage
 
 
@@ -6,9 +8,11 @@ class StripeManagementPage(BasePage):
 
     @property
     def current_plan_text(self):
-        return self.driver.find_element_by_css_selector(StripeManagementLocators.current_plan_selector)
+        # there are two possible selectors, and this seems to
 
-
-
+        if (self.driver.find_elements_by_css_selector(StripeManagementLocators.current_plan_selector)):
+            return self.driver.find_element_by_css_selector(StripeManagementLocators.current_plan_selector)
+        return self.driver.find_element_by_css_selector(StripeManagementLocators.current_plan_selector2)
 class StripeManagementLocators:
     current_plan_selector = ".Flex-alignItems--flexStart > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1) > span:nth-child(1)"
+    current_plan_selector2 = ".Flex-alignItems--flexStart > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1) > span:nth-child(1)"
