5498f3dd30217e29f9ca458d2cdde8eb9db010e2
jon blanton <jon@subless.com>
2022-08-15 11:22:38 -0400

    Added handling for cancelled HTTP requests

--- Subless.UI/sublessui/src/app/error-handling/globalerrorhandler.service.ts
+++ Subless.UI/sublessui/src/app/error-handling/globalerrorhandler.service.ts
@@ -17,6 +17,12 @@ export class GlobalerrorhandlerService implements ErrorHandler {
     else if (error["status"] && error["status"] == 410) {
       this.router.navigate(['error', 'expired']);
     }
+    else if (error["status"] && error["status"] == 0) {
+      // this seems to be related to cancelled HTTP calls, I'm going to downgrade these until we can identify a real error case
+      this.logger.warn("Error: " + error.message);
+      this.logger.warn("Stack: " + error.stack);
+      this.router.navigate(['error']);
+    }
     else {
       this.logger.error("Error: " + error.message);
       this.logger.error("Stack: " + error.stack);
