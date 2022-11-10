652d6fbc0241dc25f9dafbbd74f315aeabe345e3
Jon Blanton <jon@subless.com>
2022-11-10 16:53:21 -0500

    yanked dead key

--- Subless.UI.Tests/Keys/Keys.py
+++ Subless.UI.Tests/Keys/Keys.py
@@ -5,7 +5,6 @@ import os
 class Keys:
 
     mailslurp_api_key = os.environ['mailslurp_api_key']
-    god_token = os.environ['god_token']  # ha ha this is no longer valid
     # I assume kyle is referring to us switching from jwt based authentication to cookies that exist for a session
     # lifetime. That probably will require some re-vetting of assumptions whenever I find where this was used.
     god_password = os.environ['god_password']
