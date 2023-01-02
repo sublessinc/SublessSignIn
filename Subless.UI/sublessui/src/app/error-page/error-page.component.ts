4543206bbd77e05236a87d5209694a106b77e3da
mschechtman <matthew.schechtman@gmail.com>
2023-01-02 15:39:31 -0500

    SA-432: Update style for error page

--- Subless.UI/sublessui/src/app/error-page/error-page.component.ts
+++ Subless.UI/sublessui/src/app/error-page/error-page.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-error-page',
   templateUrl: './error-page.component.html',
-  styleUrls: ['./error-page.component.css']
+  styleUrls: ['./error-page.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ErrorPageComponent implements OnInit {
   public errorTitle: string = "You broke subless!";
-  public errorAction: string = "Tell us how by emailing contact@subless.com";
+  public errorAction: string = "Tell us how by emailing <a href='mailto:contact@subless.com'>contact@subless.com</a>";
   constructor(private route: ActivatedRoute) { }
   ngOnInit() {
     // get URL parameters
