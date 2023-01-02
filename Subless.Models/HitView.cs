500be4b9bec6b52aa6b45796f018ce9b5d2c74de
Jon Blanton <jon@subless.com>
2023-01-02 13:31:02 -0500

    Made per-partner favicons for recent activity

--- Subless.Models/HitView.cs
+++ Subless.Models/HitView.cs
@@ -7,5 +7,7 @@ namespace Subless.Models
         public Uri Content { get; set; }
         public string Title { get; set; }
         public DateTime Timestamp { get; set; }
+        public Uri Favicon { get; set; }
+        public Guid PartnerId { get; set; }
     }
 }
