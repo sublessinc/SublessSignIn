46d5b8010f90aedc2a723368f6734081c07efb7f
Jon Blanton <jon@subless.com>
2023-01-22 12:18:57 -0500

    Added test fixes

--- Subless.UI.Tests/PageObjectModels/BasePage.py
+++ Subless.UI.Tests/PageObjectModels/BasePage.py
@@ -1,5 +1,7 @@
 import logging
+import time
 
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,8 +13,16 @@ logger = logging.getLogger()
 class BasePage(object):
 
     @property
+    def logout2_button(self):
+        return self.driver.find_element_by_id(BasePageLocators.logout2_button_id)
+
+    @property
     def logout_button(self):
-        return self.driver.find_element_by_css_selector(BasePageLocators.logout_button_selector)
+        return self.driver.find_element_by_id(BasePageLocators.logout_button_id)
+
+    @property
+    def account_settings_button(self):
+        return self.driver.find_element_by_css_selector(BasePageLocators.account_settings_selector)
 
     def __init__(self, driver):
         self.driver = driver
@@ -20,20 +30,56 @@ class BasePage(object):
 
     def logout(self):
         logger.info(f'Logging Out')
-        self.wait_for_load()
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, BasePageLocators.logout_button_selector)))
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, BasePageLocators.logout_button_selector)))
-
         logger.info(self.__class__.__name__)
-        self.logout_button.click()
 
+        self.wait_for_load()
+
+        if check_exists_by_id(BasePageLocators.logout_button_id, self.driver):
+            WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, BasePageLocators.logout_button_selector)))
+            logger.info("clicking logout")
+            self.logout_button.click()
+
+        if check_exists_by_id(BasePageLocators.logout2_button_id, self.driver):
+            WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, BasePageLocators.logout2_button_selector)))
+            logger.info("clicking logout2")
+            self.logout2_button.click()
+
+        logger.info("waiting for redirect to login page")
         # wait for redirect
+        logger.info(self.driver.current_url)
         WebDriverWait(self.driver, 10).until(lambda driver: 'login' in driver.current_url)
 
     def wait_for_load(self):
         if check_exists_by_id(BasePageLocators.loading_id, self.driver):
             WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.ID, BasePageLocators.loading_id)))
 
+    def navigate_to_account_settings(self):
+        from .AccountSettingsPage import AccountSettingsPage
+        logger.info(f'Navigating to change plan')
+        time.sleep(1)
+        WebDriverWait(self.driver, 10).until(lambda driver: 'subless' in driver.current_url)
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, BasePageLocators.account_settings_selector)))
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, BasePageLocators.account_settings_selector)))
+        logger.info(self.__class__.__name__)
+        self.account_settings_button.click()
+        self.accept_alert_if_present()
+        # wait for redirect
+        WebDriverWait(self.driver, 10).until(lambda driver: 'account-settings' in driver.current_url)
+        return AccountSettingsPage(self.driver)
+
+    def accept_alert_if_present(self):
+        try:
+            WebDriverWait(self.driver, 3).until(EC.alert_is_present(),
+                                           'Timed out waiting for PA creation ' +
+                                           'confirmation popup to appear.')
+
+            alert = self.driver.switch_to.alert
+            alert.accept()
+            print("alert accepted")
+        except TimeoutException:
+            print("no alert")
 def check_exists_by_id(id, driver):
     from selenium.common.exceptions import NoSuchElementException
     try:
@@ -50,14 +96,12 @@ def check_exists_by_xpath(xpath, driver):
         return False
     return True
 
-def check_exists_by_id(id, driver):
-    from selenium.common.exceptions import NoSuchElementException
-    try:
-        driver.find_element_by_id(id)
-    except NoSuchElementException:
-        return False
-    return True
+
 
 class BasePageLocators:
-    logout_button_selector = '#logout, #logout2'
+    logout2_button_selector = '#logout2'
+    logout2_button_id = 'logout2'
+    logout_button_selector = '#logout'
+    logout_button_id = 'logout'
     loading_id = "loadingContainer"
+    account_settings_selector = "#account, #account-settings2"
--- Subless.UI.Tests/PageObjectModels/NavbarPage.py
+++ Subless.UI.Tests/PageObjectModels/NavbarPage.py
@@ -21,10 +21,6 @@ class NavbarPage(BasePage):
     def billing_button(self):
         return self.driver.find_element_by_id(NavbarPageLocators.billing_id)
 
-    @property
-    def account_settings_button(self):
-        return self.driver.find_element_by_css_selector(NavbarPageLocators.account_settings_selector)
-
     def __init__(self, driver):
         self.driver = driver
 
@@ -52,19 +48,7 @@ class NavbarPage(BasePage):
         WebDriverWait(self.driver, 10).until(lambda driver: 'plan' in self.driver.current_url)
         return StripeManagementPage(self.driver)
 
-    def navigate_to_account_settings(self):
-        from .AccountSettingsPage import AccountSettingsPage
-        logger.info(f'Navigating to change plan')
-        time.sleep(1)
-        WebDriverWait(self.driver, 10).until(lambda driver: 'subless' in driver.current_url)
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, NavbarPageLocators.account_settings_selector)))
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, NavbarPageLocators.account_settings_selector)))
-        logger.info(self.__class__.__name__)
-        self.account_settings_button.click()
 
-        # wait for redirect
-        WebDriverWait(self.driver, 10).until(lambda driver: 'account-settings' in driver.current_url)
-        return AccountSettingsPage(self.driver)
 
 class NavbarPageLocators:
     partner_profile_id = "partner"
@@ -73,4 +57,3 @@ class NavbarPageLocators:
     payout_id = "payout"
     change_plan_id = "plan"
     integration_settings_id = "integration"
-    account_settings_selector = "#account, #account-settings2"
